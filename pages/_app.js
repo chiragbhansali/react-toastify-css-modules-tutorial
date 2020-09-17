54aa66b74e8dad657d1f730f7dd7bfb5a8ca7070
Chirag Bhansali <22539632+chiragbhansali@users.noreply.github.com>
2020-09-17 17:23:19 +0530

    Add react-toastify

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+import '../styles/ReactToastify.scss';
+import { ToastContainer, Slide } from 'react-toastify';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+	return (
+		<>
+			<Component {...pageProps} />
+			<ToastContainer
+				position="bottom-right"
+				transition={Slide}
+				closeButton={false}
+			/>
+		</>
+	);
 }
-
-export default MyApp
